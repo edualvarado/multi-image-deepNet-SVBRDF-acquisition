31eddc4eff04e8c5e82643de6e9e5a51321bf56c
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-07-30 15:56:40 +0200

    CHANGED: shell file

--- testModel.sh
+++ testModel.sh
@@ -4,8 +4,8 @@
 #inputDir="testImagesExamples/"
 #checkpoint="checkpointTrained"
 
-outputDir="outputTest"
-inputDir="inputTest/"
+outputDir="outputNew"
+inputDir="inputNew/"
 checkpoint="checkpointTrained"
 
 #python pixes2Material.py --mode test --output_dir $outputDir --input_dir $inputDir --batch_size 1 --input_size 256 --nbTargets 4 --useLog --includeDiffuse --which_direction AtoB --inputMode folder --maxImages 5 --nbInputs 10 --feedMethod files --useCoordConv --checkpoint $checkpoint --fixImageNb
